b9f8780e2c57a9501ff4b94a41e6f3a443b1cc2c
Willi Kappler <willi.kappler@uni-tuebingen.de>
2018-09-13 13:41:57 +0200

    Specify server trait and server message

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,27 @@
-use std::net::{TcpListener, SocketAddr};
+// Std modules
+use std::net::{TcpListener, SocketAddr, TcpStream};
 
+// External crates
 use failure::Error;
 
+// Internal modules
 use configuration::{Configuration};
+use client::{NodeMessage};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ServerMessage {
+    ProcessData,
+    JobFinished,
+}
+
+pub trait NCServer {
+    fn node_ready_for_input();
+
+    fn node_output_data();
+
+    fn is_job_done() -> bool;
+}
+
 
 pub fn start_server(configuration: Configuration) -> Result<(), Error> {
 
@@ -12,14 +31,19 @@ pub fn start_server(configuration: Configuration) -> Result<(), Error> {
     loop {
         match listener.accept() {
             Ok((stream, address)) => {
-
+                info!("Connection from node: {}", address);
+                handle_client(stream);
             }
             Err(e) => {
-                warn!("Could not accept client connecton: {}", e)
+                warn!("Could not accept node connecton: {:?}", e)
             }
         }
     }
 
 
     Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+
 }
