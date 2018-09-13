85095f1efff7c442dc320842416a6877759dd014
Willi Kappler <willi.kappler@uni-tuebingen.de>
2018-09-13 14:59:50 +0200

    Add some crate links in comments

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,20 @@ pub mod error;
 pub mod configuration;
 pub mod server;
 pub mod client;
+
+
+/*
+    Serde overview:
+    https://serde.rs/
+
+    MessagePack:
+        https://github.com/3Hren/msgpack-rust
+        https://docs.rs/rmp-serde/0.13.7/rmp_serde/
+        https://crates.io/crates/rmp
+
+    BSON:
+        https://github.com/zonyitoo/bson-rs
+        https://crates.io/crates/bson
+
+
+*/
