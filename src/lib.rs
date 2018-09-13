9c10aa7d5c85226692ad5def3aed2bab8895d77b
Willi Kappler <willi.kappler@uni-tuebingen.de>
2018-09-13 13:41:35 +0200

    Import serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use] extern crate derive_builder;
 #[macro_use] extern crate failure;
 #[macro_use] extern crate log;
+#[macro_use] extern crate serde_derive;
+extern crate serde;
+
 
 pub mod error;
 pub mod configuration;
