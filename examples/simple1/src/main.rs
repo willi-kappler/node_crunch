eeb05a14a37d680c38c811a04931004f9e2b9e6f
Willi Kappler <willi.kappler@uni-tuebingen.de>
2018-09-24 11:40:05 +0200

    Add struct for command line options

--- examples/simple1/src/main.rs
+++ examples/simple1/src/main.rs
@@ -4,17 +4,36 @@ extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate node_crunch;
 extern crate rand;
+#[macro_use] extern crate structopt;
 
-#[derive(Debug, Deserialize)]
+mod server;
+mod node;
+
+use structopt::StructOpt;
+
+#[derive(Debug, Serialize, Deserialize)]
 struct InputData {
     chunck: usize,
     data: Vec<u8>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct OutputData {
     chunck: usize,
     data: Vec<u8>,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "simple1")]
+struct NCOpt {
+    #[structopt(short = "s", long = "server")]
+    server: bool,
+
+    #[structopt(long = "ip", default_value = "127.0.0.1")]
+    ip: String,
+
+    #[structopt(short = "p", long = "port", default_value = "2020")]
+    port: u16,
 }
 
 fn create_logger(filename: &str) {
@@ -31,5 +50,14 @@ fn create_logger(filename: &str) {
 }
 
 fn main() {
+    let options = NCOpt::from_args();
+
+    if options.server {
+        create_logger("nc_server");
+        server::run_server(options.port);
+    } else {
+        create_logger("nc_node");
+        node::run_node(&options.ip, options.port)
+    }
 
 }
