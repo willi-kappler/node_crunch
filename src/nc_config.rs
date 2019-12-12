9f948354bc384b5fcd9513c8dd9a163f2abe0c8b
Willi Kappler <willi.kappler@uni-tuebingen.de>
2019-12-12 15:48:22 +0100

    Add default values for config

--- src/nc_config.rs
+++ src/nc_config.rs
@@ -1,7 +1,19 @@
 
+
+#[derive(Debug, Clone)]
 pub struct NC_Configuration{
     pub address: String,
     pub port: u16,
     pub reconnect_wait: u64,
     // pub heartbeat_timeout: u32,
+}
+
+impl Default for NC_Configuration {
+    fn default() -> Self {
+        NC_Configuration {
+            address: "127.0.0.1".to_string(),
+            port: 9000,
+            reconnect_wait: 10,
+        }
+    }
 }
